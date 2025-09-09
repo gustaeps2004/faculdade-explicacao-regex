9afcd5e0d2702f64690f1e853586329db3295eb9
gustaeps2004 <gustaeps@gmail.com>
2025-09-09 20:16:14 -0300

    adicionando modal de explicação

---
+++ js/modalExplicacao.js
@@ -0,0 +1,51 @@
+const modalExplicacao = document.getElementById('modal-explicacao');
+const stepsExplicacao = document.querySelectorAll('.step-explicacao');
+const openBtnExplicacao = document.getElementById('openModalExplicacao');
+const closeBtnExplicacao = document.getElementById('closeModalBtnExplicacao');
+const nextBtnsExplicacao = document.querySelectorAll('.next-btn-explicacao');
+const prevBtnsExplicacao = document.querySelectorAll('.prev-btn-explicacao');
+const closeBtnExplicacaoModal = document.querySelector('.close-btn-explicacao-modal');
+
+let currentStepExplicacao = 0;
+
+function showStepExplicacao(index) {
+  stepsExplicacao.forEach((step, i) => {
+    step.classList.toggle('active', i === index);
+  });
+}
+
+closeBtnExplicacao.addEventListener('click', () => {
+  closeModal()
+});
+
+openBtnExplicacao.addEventListener('click', () => {
+  modalExplicacao.style.display = 'block';
+  showStepExplicacao(currentStepExplicacao);
+});
+
+nextBtnsExplicacao.forEach(btn => {
+  btn.addEventListener('click', () => {
+    if (currentStepExplicacao < steps.length - 1) {
+      currentStepExplicacao++;
+      showStepExplicacao(currentStepExplicacao);
+    }
+  });
+});
+
+prevBtnsExplicacao.forEach(btn => {
+  btn.addEventListener('click', () => {
+    if (currentStepExplicacao > 0) {
+      currentStepExplicacao--;
+      showStepExplicacao(currentStepExplicacao);
+    }
+  });
+});
+
+closeBtnExplicacaoModal.addEventListener('click', () => {
+  closeModal()
+});
+
+function closeModal() {
+  modalExplicacao.style.display = 'none';
+  currentStepExplicacao = 0;
+}
