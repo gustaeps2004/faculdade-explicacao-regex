380f0b46beea77e75e344c13451d04f3c94a8747
emanueloliveirasenai <emanuel_oliveira151@estudante.sesisenai.org.br>
2025-09-09 21:56:56 -0300

    Validação do endereço no formulário

--- js/modalFormulario.js
+++ js/modalFormulario.js
@@ -47,3 +47,54 @@ finishBtn.addEventListener('click', () => {
   modal.style.display = 'none';
   currentStep = 0;
 });
+
+function validarDadosEndereco(){
+
+  const cep = document.getElementById('cep').value;
+
+  if(/^\d{8}$/.test(cep) == false){
+    alert("Preencha o campo CEP corretamente")
+    document.getElementById('cep').focus();
+    return false;
+  }
+
+  const logradouro = document.getElementById('logradouro').value;
+
+  if(logradouro == ""){
+    alert("Preencha o campo Logradouro");
+    document.getElementById('logradouro').focus();
+    return false;
+  }
+
+  const numero = document.getElementById('numero').value;
+
+  if(/^\d{1,5}$/.test(numero) == false){
+    alert("Preencha o campo Número corretamente");
+    document.getElementById('numero').focus();
+    return false;
+  }
+
+  const complemento = document.getElementById('complemento').value;
+
+  if(/^[A-Za-z]+$/.test(complemento) == false){
+    alert("Preencha o campo Complemento corretamente");
+    document.getElementById('complemento').focus();
+    return false;
+  }
+
+  const cidade = document.getElementById('cidade').value;
+
+  if(/^[A-Za-zÀ-ÿ]+([ ][A-Za-zÀ-ÿ]+)*$/.test(cidade) == false){
+    alert("Preencha o campo Cidade corretamente");
+    document.getElementById('cidade').focus();
+    return false;
+  }
+
+  const estado = document.getElementById('estado').value;
+
+  if(/^[A-Za-zÀ-ÿ]+([ ][A-Za-zÀ-ÿ]+)*$/.test(estado) == false){
+    alert("Preencha o campo Estado corretamente");
+    document.getElementById('estado').focus();
+    return false;
+  }
+}
